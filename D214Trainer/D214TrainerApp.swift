249095d54be219d97f2459e49ac0e9a6bcac805c
AAlchi <alialchi07@gmail.com>
2025-03-20 17:27:38 -0500

    Initial Commit

---
+++ D214Trainer/D214TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  D214TrainerApp.swift
+//  D214Trainer
+//
+//  Created by Ali Alchikh Ibrahim on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct D214TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
