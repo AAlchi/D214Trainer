1e1bcc86f88184294315a0695cc280c0a24b584d
Fatima Mustakova <fmu1004@gmail.com>
2025-04-12 21:56:26 -0500

    Changed background color, increased font sizes, and rearranged button/label placements.

--- D214Trainer/WelcomeView.swift
+++ D214Trainer/WelcomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Color.red
+        Color(red: 196/255, green: 48/255, blue: 18/255)
             .ignoresSafeArea()
             .overlay {
                 
@@ -17,61 +17,64 @@ struct ContentView: View {
                     
                     Text("Good morning!").foregroundColor(.white)
                         .multilineTextAlignment(.center)
-                        .font(.largeTitle.weight(.bold))
+                        .font(.system(size:60))
+                        .bold()
                         .padding()
+                        .position(x: 210, y: 200)
                         
                     
                     
                     Text("SELECT YOUR TASK").foregroundColor(.white)
                         .multilineTextAlignment(.center)
-                        .font(.system(size:30))
+                        .font(.system(size:32))
+                        .bold()
+                        .position(x: 195, y:100)
                     
-                }
-                
-                
-                VStack(alignment: .leading) {
+              
                     
                     Button(action: {
                         print("Clicked")
                             
                         
                     }) {
-                        Text("Soft Skills").foregroundColor(.red)
+                        Text("   Soft Skills          ").foregroundColor(Color(red: 196/255, green: 48/255, blue: 18/255))
                             .font(.title)
                             .multilineTextAlignment(.center)
                             .border(.black)
                             .background(Color.white)
+                            .background().frame(width: 450, height: 75)
                             .fontWeight(.bold)
                             .padding(10)
-                            .position(x: 150, y: 300)
+                            .position(x: 200, y: 175)
                             
                         
                         
                         
                         
                         }
-                    .frame(width:300, height: 300, alignment: .trailing)
+                    .frame(width:1200, height: 450, alignment: .trailing)
                 }
                 
                 
-                VStack {
+                
+                VStack(alignment: .leading) {
                     Button(action: {
-                        print("Clicked")
+                        print("Clicked Again")
                         
                     }) {
-                        Text("Hard Skills").foregroundColor(.red)
+                        Text("   Hard Skills         ").foregroundColor(Color(red: 196/255, green: 48/255, blue: 18/255))
                             .background(Color.white)
                             .border(.black)
                             .font(.title)
                             .fontWeight(.bold)
                             .padding(10)
-                            .position(x: 150, y: 250)
+                            .position(x: 200, y: 250)
                             
                         
                         
                     }
                 }
-                .frame(width:300, height: 300, alignment: .trailing)
+                .frame(width:1200, height: 500, alignment: .trailing)
             }
         
         
