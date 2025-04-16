aebf2a4b8e6462d5d7fc39c8ba04bcbc109b11a4
AAlchi <alialchi07@gmail.com>
2025-04-16 14:56:30 -0500

    Fixed formatting

--- D214Trainer/WelcomeView.swift
+++ D214Trainer/WelcomeView.swift
@@ -7,15 +7,13 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WelcomeView: View {
     var body: some View {
         
         Color(red: 196/255, green: 48/255, blue: 18/255)
             .ignoresSafeArea()
             .overlay {
-                
                 VStack(alignment: .leading) {
-                    
                     Text("Good morning!").foregroundColor(.white)
                         .multilineTextAlignment(.center)
                         .font(.system(size:61))
@@ -23,20 +21,14 @@ struct ContentView: View {
                         .padding()
                         .position(x: 210, y: 200)
                     
-                    
-                    
                     Text("SELECT YOUR TASK").foregroundColor(.white)
                         .multilineTextAlignment(.center)
                         .font(.system(size:32))
                         .bold()
                         .position(x: 195, y:100)
                     
-                    
-                    
                     Button(action: {
                         print("Clicked")
-                        
-                        
                     }) {
                         Text("   Soft Skills          ").foregroundColor(Color(red: 196/255, green: 48/255, blue: 18/255))
                             .font(.title)
@@ -48,22 +40,13 @@ struct ContentView: View {
                             .padding(10)
                             .position(x: 200, y: 175)
                             .cornerRadius(11)
-                        
-                        
-                        
-                        
-                        
                     }
                     .frame(width:1200, height: 450, alignment: .trailing)
                 }
                 
-                
-                
                 VStack(alignment: .leading) {
                     Button(action: {
                         print("Clicked Again")
-                        
-                        
                     }) {
                         Text("   Hard Skills         ").foregroundColor(Color(red: 196/255, green: 48/255, blue: 18/255))
                             .background(Color.white)
@@ -73,23 +56,17 @@ struct ContentView: View {
                             .fontWeight(.bold)
                             .padding(10)
                             .position(x: 200, y: 250)
-                        
-                        
-                        
                     }
                 }
                 .frame(width:1200, height: 500, alignment: .trailing)
             }
     }
 }
-        
-        struct ContentView_Previews: PreviewProvider {
-            static var previews: some View {
-                ContentView()
-                
-                
-                
-            }
-        }
-    
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
+
 
