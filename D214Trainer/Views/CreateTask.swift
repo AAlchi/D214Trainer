f85ed4a5c885f78ba9e8a716c260eece142f75b3
AAlchi <alialchi07@gmail.com>
2025-04-16 15:02:04 -0500

    Structure complete for create page

---
+++ D214Trainer/Views/CreateTask.swift
@@ -0,0 +1,22 @@
+//
+//  CreateTask.swift
+//  D214Trainer
+//
+//  Created by Ali Alchikh Ibrahim on 4/16/25.
+//
+
+import SwiftUI
+
+struct CreateTask: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Text("Let's create a task!")
+            }
+            
+            VStack {
+                
+            }
+        }
+    }
+}
