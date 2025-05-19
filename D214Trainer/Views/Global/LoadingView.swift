c0034ef8961de5f8c7677856a139b2f48a5d3ae2
AAlchi <alialchi07@gmail.com>
2025-05-19 10:04:49 -0500

    Final changes for now

---
+++ D214Trainer/Views/Global/LoadingView.swift
@@ -0,0 +1,40 @@
+//  LoadingView.swift
+//  D214Trainer
+//
+//  Created by Amir Eskarous on 3/26/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    @Binding var showing: Bool
+    
+    var body: some View {
+        NavigationStack {
+            ZStack{
+                Color(red: 186/255, green: 12/255, blue: 47/255)
+                    .ignoresSafeArea()
+                VStack {
+                    HStack {
+                        Spacer()
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: Color(red: 120/255, green: 180/255, blue: 255/255)))
+                            .scaleEffect(1)
+                            .foregroundStyle(.white)
+                            .padding()
+                    }
+                    Spacer()
+                }
+                Image("BrewEd")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    showing = false
+                }
+            }
+        }
+    }
+}
--- D214Trainer/Views/WelcomeView.swift
+++ D214Trainer/Views/WelcomeView.swift
@@ -8,67 +8,73 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State var show = true
+    
     var body: some View {
-        NavigationStack {
-            GeometryReader { geo in
-                ZStack {
-                    HStack {
-                        Spacer()
-                        Image("img2")
-                            .resizable()
-                            .frame(maxHeight: .infinity)
-                            .aspectRatio(contentMode: .fit)
-                    }
-                    
-                    HStack {
-                        VStack(alignment: .leading) {
-                            VStack(alignment: .leading) {
-                                Text("Good morning!")
-                                    .padding(.vertical, 10)
-                                    .foregroundColor(.white)
-                                    .bold()
-                                    .font(.system(size: 61))
-                                
-                                
-                                Text("SELECT YOUR TASK")
-                                    .padding(.bottom)
-                                    .foregroundColor(.white)
-                                    .font(.system(size:32))
-                            }
-                            
-                            NavigationLink(destination: SearchPage(skillType: .constant("hard"))) {
-                                Text("Hard Skills")
-                                    .frame(maxWidth: .infinity)
-                                    .padding()
-                                    .foregroundColor(Color(red: 194/255, green: 23/255, blue: 58/255))
-                                    .background(Color(.white))
-                                    .bold()
-                                    .font(.system(size:30))
-                            }
-                            .cornerRadius(10)
-                            
-                            NavigationLink(destination: SearchPage(skillType: .constant("soft"))) {
-                                Text("Soft Skills")
-                                    .frame(maxWidth: .infinity)
-                                    .padding()
-                                    .foregroundColor(Color(red: 194/255, green: 23/255, blue: 58/255))
-                                    .background(Color(.white))
-                                    .bold()
-                                    .font(.system(size:30))
-                            }
-                            .cornerRadius(10)
-                            
+        if (show == true) {
+            LoadingView(showing: $show)
+        } else {
+            NavigationStack {
+                GeometryReader { geo in
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            Image("img2")
+                                .resizable()
+                                .frame(maxHeight: .infinity)
+                                .aspectRatio(contentMode: .fit)
                         }
-                        .frame(width: geo.size.width * 0.4)
-                        .padding(.horizontal)
-                        .padding(.horizontal)
                         
-                        Spacer()
+                        HStack {
+                            VStack(alignment: .leading) {
+                                VStack(alignment: .leading) {
+                                    Text("Good morning!")
+                                        .padding(.vertical, 10)
+                                        .foregroundColor(.white)
+                                        .bold()
+                                        .font(.system(size: 61))
+                                    
+                                    
+                                    Text("SELECT YOUR TASK")
+                                        .padding(.bottom)
+                                        .foregroundColor(.white)
+                                        .font(.system(size:32))
+                                }
+                                
+                                NavigationLink(destination: SearchPage(skillType: .constant("hard"))) {
+                                    Text("Hard Skills")
+                                        .frame(maxWidth: .infinity)
+                                        .padding()
+                                        .foregroundColor(Color(red: 194/255, green: 23/255, blue: 58/255))
+                                        .background(Color(.white))
+                                        .bold()
+                                        .font(.system(size:30))
+                                }
+                                .cornerRadius(10)
+                                
+                                NavigationLink(destination: SearchPage(skillType: .constant("soft"))) {
+                                    Text("Soft Skills")
+                                        .frame(maxWidth: .infinity)
+                                        .padding()
+                                        .foregroundColor(Color(red: 194/255, green: 23/255, blue: 58/255))
+                                        .background(Color(.white))
+                                        .bold()
+                                        .font(.system(size:30))
+                                }
+                                .cornerRadius(10)
+                                
+                            }
+                            .frame(width: geo.size.width * 0.4)
+                            .padding(.horizontal)
+                            .padding(.horizontal)
+                            
+                            Spacer()
+                        }
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color(red: 194/255, green: 23/255, blue: 58/255))
+                    .ignoresSafeArea()
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color(red: 194/255, green: 23/255, blue: 58/255))
-                .ignoresSafeArea()
             }
         }
     }
